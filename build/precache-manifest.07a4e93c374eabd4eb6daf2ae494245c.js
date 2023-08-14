48e46fa239d4133a8534628ae5c91c868b45e175
shayan mahmood offical! / <76242026+shayanmahmood@users.noreply.github.com>
2023-08-14 01:21:21 -0700

    VEnue
    Depolyed

---
+++ build/precache-manifest.07a4e93c374eabd4eb6daf2ae494245c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "296314dae27a7cd37f18",
+    "url": "/static/css/main.5f12846f.chunk.css"
+  },
+  {
+    "revision": "296314dae27a7cd37f18",
+    "url": "/static/js/main.296314da.chunk.js"
+  },
+  {
+    "revision": "a6145e3848c94c52be99",
+    "url": "/static/js/1.a6145e38.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "8d03ca2f1f7fdce9801ec7e2384f231e",
+    "url": "/index.html"
+  }
+];
